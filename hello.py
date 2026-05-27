5c4e5b75c00c0ad13a48fd89d3652fa922fb1cb1
riyazchhabra <riyazchhabra5@gmail.com>
2026-05-27 21:39:48 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+#print("hello world", end=" ");
+#print("hi")
+#print("hello world \n hi")
+
+#a=input("What is your name?")
+#print("hello "+a)
+#print(len(input("what is your age ")))
+
+print("Welcome to the Band Name Generator!")
+a=input("What is the name of the city you grew up in? \n")
+b=input("What is your pet name? \n")
+print("The name of you band is " + a +" "+  b)
+
