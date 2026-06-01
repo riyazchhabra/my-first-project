aaf214463c28c1f0b1f53d6558b6daae5fb66a44
riyazchhabra <riyazchhabra5@gmail.com>
2026-06-01 19:10:28 +0530

    if_else.py

--- if_else.py
+++ if_else.py
@@ -16,6 +16,36 @@ if height>120:
     want_photo=input("Do you want to have photos while rollercoaster? Type 'y' for yes and 'n' for no.")
     if want_photo=='y':
         bill+=3
+    elif want_photo=="n":
+        bill=bill
+    else:
+        print("You typed wrong input")
     print(f"Your final bill is ${bill}")
+        
 else:
     print("You cannot ride the rollercoaster!")
+
+input(print(" -------------------------------------------------------------------------"))
+
+print("Welcome to Python Pizza Deliveries!")
+bill=0
+size=input("Which size of pizza would you prefer? 'S' for small 'M' for mediun 'L' for large: ")
+pepperoni=input("Would you like pepperoni on your pizza? 'Yes' or 'No': ")
+if size=="S":
+    bill=15
+    if pepperoni=="Yes":
+        bill=+2
+elif size=="M":
+    bill=20
+    if pepperoni=="Yes":
+        bill+=4
+elif size=="L":
+    bill=25
+    if pepperoni=="Yes":
+        bill+=4
+else:
+    print("You typed wrong inputs")
+cheese=input("Would you like extra cheese on your pizza? 'Yes' or 'No': ")
+if cheese=="Yes":
+    bill+=1
+total_bill=print(f"Your total bill is ${bill}")
